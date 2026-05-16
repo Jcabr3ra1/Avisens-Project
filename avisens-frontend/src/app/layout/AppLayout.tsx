a634b58ef49657b785f736a1f1a808a92f3b1727
Juan Jaller <juancabrera1293@gmail.com>
2026-05-16 00:12:39 -0500

    Revert "feat: migrate AppLayout, LoginPage and LoginForm to Tailwind"
    This reverts commit 2cb80e147cffb32a46f87f2c183d623c3c321d03.

--- avisens-frontend/src/app/layout/AppLayout.tsx
+++ avisens-frontend/src/app/layout/AppLayout.tsx
@@ -2,13 +2,14 @@ import { Outlet } from 'react-router-dom'
 import Navbar from './Navbar/Navbar'
 import Footer from './Footer/Footer'
 import FloatChat from './FloatChat/FloatChat'
+import './AppLayout.css'
 
 function AppLayout() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="app-layout">
       <Navbar />
-      <div className="flex-1">
-        <main className="relative z-[1]">
+      <div className="app-content">
+        <main className="app-main">
           <Outlet />
         </main>
       </div>
